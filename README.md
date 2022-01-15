7e37365fd01c1ba2dc543f662c9483beefc6f18c
EthanJia <181716254@qq.com>
2022-01-16 01:42:07 +0800

    feat: requests

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # Label Studio Pipeline
 
-This is a [label studio](https://github.com/heartexlabs/label-studio) pipeline designed by _Ethan Jia_, licensed by MIT license.
+This is a [Label Studio](https://github.com/heartexlabs/label-studio) pipeline designed by _Ethan Jia_, licensed by MIT license.
 
 ## Installation
 
-- Start label studio.
+- Start Label Studio:
+
+```bash
+# Install all package dependencies
+pip3 install -e .
+# Run database migrations
+python3 label_studio/manage.py migrate
+# Start the server in development mode at http://localhost:8080
+python3 label_studio/manage.py runserver
+```
+
 - Clone this repo.
-- `pip3 install label-studio-sdk`
+- Install the SDK for Label Studio:
+
+```bash
+pip3 install label-studio-sdk
+```
 
 ## Usage
 
--- uploader.py
+++ uploader.py
@@ -1,11 +1,33 @@
 # Import the SDK and the client module
 from label_studio_sdk import Client
+import requests as rq
+import os
 
 # Define the URL where Label Studio is accessible and the API key for your user account
 LABEL_STUDIO_URL = 'http://localhost:8080'
 API_KEY = '181439286e4b2ed9c0026f5e46a27a39858e6905'
+IMG_PATH = './img/'
 
 # Connect to the Label Studio API and check the connection
-ls = Client(url=LABEL_STUDIO_URL, api_key=API_KEY)
-ls.check_connection()
-print(ls.get_projects())
+lbsd = Client(url=LABEL_STUDIO_URL, api_key=API_KEY)
+if not lbsd.check_connection()['status'] == 'UP':
+    print('Warning: Connection Fails!')
+else:
+    print('Connection Succeeds!')
+    print('')
+    # Upload the images in ./img
+    for root, dir, file in os.walk(IMG_PATH):
+        img_dir = file
+        break
+    headers = {
+        'Authorization': 'Token 181439286e4b2ed9c0026f5e46a27a39858e6905',
+    }
+    response_list = []
+    for img in img_dir:
+        files = {
+            'FileUpload': (IMG_PATH+img, open(IMG_PATH+img, 'rb')),
+        }
+        response = rq.post('http://localhost:8080/api/projects/1/import',
+                           headers=headers, files=files)
+        response_list.append(response)
+    # print(response_list)
