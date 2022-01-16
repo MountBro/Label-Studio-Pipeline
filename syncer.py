26552c3a630274dcd7135373ee430b3062f9b0d4
EthanJia <181716254@qq.com>
2022-01-16 21:48:43 +0800

    feat: try sync local storage

--- syncer.py
+++ syncer.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import date
 import json
 import os
 import requests as rq
@@ -15,26 +16,28 @@ def main():
     # Connect to the Label Studio API and check the connection
     lbsd = Client(url=LABEL_STUDIO_URL, api_key=API_KEY)
     if not lbsd.check_connection()['status'] == 'UP':
-        print('Warning: Connection Fails!')
+        # print('Warning: Connection Fails!')
+        pass
     else:
-        print('Connection Succeeds!')
-    headers = {
-        'Authorization': 'Token 181439286e4b2ed9c0026f5e46a27a39858e6905',
-        'Content-Type': 'application/json',
-    }
-    data = {
-        "path": "./img/",
-        "regex_filter": "",
-        "use_blob_urls": True,
-        "title": "test sync",
-        "description": "test sync",
-        "last_sync": "",
-        "last_sync_count": 0,
-        "project": 0
-    }
-    response = rq.post(
-        'http://localhost:8080/api/projects/1/import', headers=headers, data=json.dumps(data, separators=(',', ':')))
-    print(response.json())
+        # print('Connection Succeeds!')
+        headers = {
+            'Authorization': 'Token 181439286e4b2ed9c0026f5e46a27a39858e6905',
+            'Content-Type': 'application/json',
+        }
+        data = {
+            "path": "/home/ethanjia/dev/label-studio-pipeline/img",
+            "regex_filter": "",
+            "use_blob_urls": True,
+            "title": "test sync",
+            "description": "test sync",
+            "last_sync": "2019-08-24T14:15:22Z",
+            "last_sync_count": 0,
+            "project": 1
+        }
+        # FIXME 这里的id不知道是啥
+        response = rq.post(
+            'http://localhost:8080/api/storages/localfiles/{id}/sync', headers=headers, data=json.dumps(data, separators=(',', ':')))
+        print(response.json())
 
 
 if __name__ == '__main__':
